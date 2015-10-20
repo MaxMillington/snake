a15f01edac61134558f39465436eb14b97de5c58
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-10-20 15:20:33 -0600

    bodyPart object exists test passing

--- test/snake-test.js
+++ test/snake-test.js
@@ -1,19 +1,23 @@
 const chai = require('chai');
 const assert = chai.assert;
-const Snake = require('../lib/snake');
+const BodyPart = require('../lib/snake');
 
 describe('my test suite', function () {
-    it('should work', function () {
-        assert(true);
-    });
+  it('should work', function () {
+      assert(true);
+  });
 });
 
 describe('Snake', function () {
-    it('should instantiate with an x, y, height, and width', function(){
-        let bodyPart = new BodyPart({ x: 20, y: 20, width: 30, height: 30});
-        assert.equal(bodyPart.x, 20);
-        assert.equal(bodyPart.width, 30);
-        assert.equal(bodyPart.height, 30);
-        assert.equal(bodyPart.y, 20);
-    });
+  it('exists', function () {
+    assert(new BodyPart);
+  });
+
+  // it('should instantiate with an x, y, height, and width', function(){
+  //     let bodyPart = new BodyPart(20, 20, 30, 30);
+  //     assert.equal(bodyPart.x, 20);
+  //     assert.equal(bodyPart.width, 30);
+  //     assert.equal(bodyPart.height, 30);
+  //     assert.equal(bodyPart.y, 20);
+  // });
 });
