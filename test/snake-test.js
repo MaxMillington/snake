983762a11a3bee5c000efd79860e502ef64a0746
MaxMillington <maxmillington89@gmail.com>
2015-10-20 15:05:54 -0600

    loading the files in the test but the functions are not loading correctly

--- test/snake-test.js
+++ test/snake-test.js
@@ -1,12 +1,13 @@
 const chai = require('chai');
 const assert = chai.assert;
-const Snake = require('../lib/index');
+const Snake = require('../lib/snake');
 
 describe('my test suite', function () {
     it('should work', function () {
         assert(true);
     });
 });
+
 describe('Snake', function () {
     it('should instantiate with an x, y, height, and width', function(){
         let bodyPart = new BodyPart({ x: 20, y: 20, width: 30, height: 30});
