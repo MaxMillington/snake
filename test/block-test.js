ec215042dd632b831ec50521db63457083bff9a6
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-10-21 15:31:07 -0600

    block has access to boards y-coordinant

--- test/block-test.js
+++ test/block-test.js
@@ -15,8 +15,13 @@ describe('Block', function () {
     assert.equal(block.board, this.board);
   });
 
-  it('should have access to a Board X-coordinate', function () {
+  it('should have access to a Boards X-coordinate', function () {
     let block = this.board.addBlock(10, 10);
     assert.equal(block.x, 10);
+  });
+
+  it('should have access to a Boards Y-coordinate', function () {
+    let block = this.board.addBlock(10, 10);
+    assert.equal(block.y, 10);
   });
 });
