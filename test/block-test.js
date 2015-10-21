2b1ba02573529d4e30a395a78d20a5ad1faabb2a
Dmitry Vizersky <dmitry.vizer@gmail.com>
2015-10-21 17:21:05 -0600

    should be false at the bottom of board

--- test/block-test.js
+++ test/block-test.js
@@ -16,17 +16,34 @@ describe('Block', function () {
   });
 
   it('should have access to a Boards X-coordinate', function () {
-    let block = this.board.addBlock(10, 10);
-    assert.equal(block.x, 10);
+    let block = this.board.addBlock(100, 100);
+    assert.equal(block.x, 100);
   });
 
   it('should have access to a Boards Y-coordinate', function () {
-    let block = this.board.addBlock(10, 10);
-    assert.equal(block.y, 10);
+    let block = this.board.addBlock(100, 100);
+    assert.equal(block.y, 100);
   });
 
   it('should be a member of the board passed in', function () {
-    let block = this.board.addBlock(5, 5);
+    let block = this.board.addBlock(50, 50);
     assert.include(this.board.blocks, block);
   });
+});
+
+describe('canMoveDown', function () {
+  beforeEach(function () {
+    this.board = new Board(300, 200);
+  });
+
+  it('should be true if it can move down', function () {
+    let block = this.board.addBlock(10, 100);
+    assert.equal(block.canMoveDown(), true);
+  });
+
+  it('should be false if at the bottom edge of the board', function () {
+    let block = this.board.addBlock(10, 200);
+    assert.equal(block.canMoveDown(), false);
+  });
+
 });
