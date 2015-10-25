07e20fd2e5b229c5c27024f67b223a1beadca6c2
MaxMillington <maxmillington89@gmail.com>
2015-10-25 14:33:22 -0600

    made it so that the food instantiates with a random location

--- test/food-test.js
+++ test/food-test.js
@@ -8,6 +8,22 @@ const Food     = require('../lib/food').Food;
 
 describe('Food', function () {
     it('exists', function () {
-        assert(new Food())
+        let board = new Board(600, 600);
+        assert(new Food(board));
+    });
+
+    it('has a location', function () {
+        let board = new Board(600, 600);
+        let food = new Food(board);
+        assert(food.x);
+        assert(food.y);
+    });
+
+    it('instantiates at a random location', function () {
+        let board = new Board(600, 600);
+        let food1 = new Food(board);
+        let food2 = new Food(board);
+        assert.notDeepEqual(food1, food2);
     })
-})
+});
+
